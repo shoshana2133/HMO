9930de3811330d3db70aa3921a76d20410b43e3d
shoshana2133 <s0527183541@gmail.com>
2024-03-28 18:37:12 +0200

    Adding a database

---
+++ HMO/API/BLL/Interfaces/ImemberBLL.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Models;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ImemberBLL
+    {
+        List<MemberDTO> GetAllMemberBll();
+        bool DeleteMemberBll(int id_mem);
+        int AddMemberBll(MemberDTO member);
+        bool UpdateMemberBll(MemberDTO member);
+
+    }
+}
---
+++ HMO/API/DTO/Mapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Mapper:Profile
+    {
+        
+        public Mapper()
+        {
+            CreateMap<MemberHmo, MemberDTO>();
+            CreateMap<MemberDTO, MemberHmo>();
+            CreateMap<VaccinatedMbr, VaccinatedMbrDto>()
+                .ForMember(dest => dest.VcManufacturer, opt => opt.MapFrom(src => src.VcCodeNavigation.VcManufacturer));
+            CreateMap<VaccinatedMbrDto, VaccinatedMbr>();
+            CreateMap<Vaccination, VaccinationDTO>();
+            CreateMap<VaccinationDTO, Vaccination>();
+            CreateMap<CoronaPatientDTO, CoronaPatient>();
+            CreateMap<CoronaPatient, CoronaPatientDTO>();
+        }
+    }
+}
